50d5f261c4e87932dbc59f5b2dd2dffe08ff90b3
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-06-22 19:46:10 +0200

    fix lint warnings

--- src/Tests/User.test.js
+++ src/Tests/User.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import User from '../Components/User/User';
 
