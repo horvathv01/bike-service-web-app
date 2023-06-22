7bde20473cd060fe15e0e8b9cd1248926e8953cd
Krisztian Molnar <111390697+KrisztanZero@users.noreply.github.com>
2023-06-22 09:46:25 +0200

    update User.test.js

--- src/Tests/User.test.js
+++ src/Tests/User.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import User from '../Components/User/User';
 
 describe('User component', () => {
   test('renders User component correctly', () => {
-    const { getByTestId } = render(<BrowserRouter><User /></BrowserRouter>);
-    const headerElement = getByTestId('header');
-    const userDisplayElement = getByTestId('user-display');
+    render(<BrowserRouter><User /></BrowserRouter>);
+    const headerElement = screen.getByTestId('header');
+    const userDisplayElement = screen.getByTestId('user-display');
 
     expect(headerElement).toBeInTheDocument();
     expect(userDisplayElement).toBeInTheDocument();
